4e26d8df94341d303eabb400bc13d2a4c30fc86c
Irene San Roman <irenesrfuentes@gmail.com>
2024-11-26 14:55:55 +0100

    Primeros Pasos

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent {
+
+  public myForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0)
+
+  });
+
+
+  onSave():void{
+    console.log(this.myForm.value);
+  }
+
+}
